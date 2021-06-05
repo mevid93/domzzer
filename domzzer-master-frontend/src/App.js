55986b11a79431da039dda56f38f1391f187b9ce
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-06-05 18:17:19 +0300

    Navigation bar to master frontend homepage

--- domzzer-master-frontend/src/App.js
+++ domzzer-master-frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom"
 
 function App() {
+
+  const padding = {
+    padding: 5
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+          <Link style={padding} to="/">home</Link>
+          <Link style={padding} to="/slaves">slaves</Link>
+          <Link style={padding} to="/vulnerabilities">vulnerabilities</Link>
+          <Link style={padding} to="/settings">settings</Link>
+        </div>
+
+        <Switch>
+          <Route path="/settings">
+
+          </Route>
+          <Route path="/vulnerabilities">
+
+          </Route>
+          <Route path="/slaves">
+
+          </Route>
+          <Route path="/">
+
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
