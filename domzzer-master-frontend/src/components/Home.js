51aabb86c464536d798992a360dea004d28eff26
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-06-06 12:37:46 +0300

    Added some css styles to frontend home page (Materila UI)

--- domzzer-master-frontend/src/components/Home.js
+++ domzzer-master-frontend/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { serverInfoChange } from '../reducers/ServerInfoReducer'
 import serverInfoService from '../services/ServerInfoService'
+import { TableContainer, Table, TableBody, TableRow, TableCell } from '@material-ui/core/'
 
 const ServerInfo = ({ serverInfo }) => {
   if (serverInfo === undefined || serverInfo.serverName === undefined) {
@@ -12,17 +13,73 @@ const ServerInfo = ({ serverInfo }) => {
     )
   }
   return (
-    <div>
-      <p>Master-server name: {serverInfo.serverName}</p>
-      <p>Master-server OS: {serverInfo.serverType}</p>
-      <p>Master-server OS-version: {serverInfo.serverVersion}</p>
-      <p>Master-server memory: {serverInfo.serverMemoryMb} Mb</p>
-      <p>Master-server date: {serverInfo.serverDate}</p>
-      <p>Master-server uptime: {serverInfo.serverUptime}</p>
-      <p>Number of slave machines: {serverInfo.numberOfSlaves}</p>
-      <p>Number of tests performed: {serverInfo.numberOfTestsPerformed}</p>
-      <p>Number of potential vulnerabilities found: {serverInfo.numberOfPotentialVulnerabilities}</p>
-    </div>
+    <TableContainer>
+      <Table aria-label="simple table">
+        <TableBody>
+          <TableRow key="1">
+            <TableCell align="left">Server name:</TableCell>
+            <TableCell align="left">{serverInfo.serverName}</TableCell>
+          </TableRow>
+          <TableRow key="2">
+            <TableCell align="left">Server OS:</TableCell>
+            <TableCell align="left">{serverInfo.serverType}</TableCell>
+          </TableRow>
+          <TableRow key="3">
+            <TableCell align="left">Server OS-version:</TableCell>
+            <TableCell align="left">{serverInfo.serverVersion}</TableCell>
+          </TableRow>
+          <TableRow key="4">
+            <TableCell align="left">Server memory:</TableCell>
+            <TableCell align="left">{serverInfo.serverMemoryMb}</TableCell>
+          </TableRow>
+          <TableRow key="5">
+            <TableCell align="left">Server date:</TableCell>
+            <TableCell align="left">{serverInfo.serverDate}</TableCell>
+          </TableRow>
+          <TableRow key="6">
+            <TableCell align="left">Server uptime:</TableCell>
+            <TableCell align="left">{serverInfo.serverUptime}</TableCell>
+          </TableRow>
+          <TableRow key="7">
+            <TableCell align="left">Server controlled slave machines:</TableCell>
+            <TableCell align="left">{serverInfo.numberOfSlaves}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+const VulnerabilityInfo = ({ serverInfo }) => {
+  if (serverInfo === undefined || serverInfo.serverName === undefined) {
+    return (
+      <div>
+        <p>Server information not available!</p>
+      </div>
+    )
+  }
+  const tests = serverInfo.numberOfTestsPerformed
+  const vulnerabilities = serverInfo.numberOfPotentialVulnerabilities
+  const ratio = (tests !== undefined && tests !== 0) ? vulnerabilities / tests : 0
+  return (
+    <TableContainer>
+      <Table aria-label="simple table">
+        <TableBody>
+          <TableRow key="1">
+            <TableCell align="left">Number of tests performed:</TableCell>
+            <TableCell align="right">{tests}</TableCell>
+          </TableRow>
+          <TableRow key="2">
+            <TableCell align="left">Number of potential vulnerabilities found:</TableCell>
+            <TableCell align="right">{vulnerabilities}</TableCell>
+          </TableRow>
+          <TableRow key="3">
+            <TableCell align="left">Potential vulnerabilities to tests ratio:</TableCell>
+            <TableCell align="right">{ratio}</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
 
@@ -40,7 +97,11 @@ const Home = () => {
   return (
     <div>
       <h1>domzzer - Home</h1>
+      <h3>Master Server Information</h3>
       <ServerInfo serverInfo={serverInfo} />
+      <br />
+      <h3>Vulnerability Information</h3>
+      <VulnerabilityInfo serverInfo={serverInfo} />
     </div>
   )
 }
