68f1e745262fb8820e0b20fce7f10cd2b55d9a76
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-06-19 11:21:19 +0300

    master frontend can now filter slaves by keyword

--- domzzer-master-frontend/src/components/Slaves.js
+++ domzzer-master-frontend/src/components/Slaves.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from 'react-redux'
 import { slavesChange } from '../reducers/SlaveReducer'
 import { errorMsgChange } from '../reducers/ErrorMsgReducer'
 import slaveService from '../services/SlaveService'
 import { TableContainer, Table, TableHead, TableBody, TableRow, TableCell, Button } from '@material-ui/core/'
+import TextField from '@material-ui/core/TextField'
+import Grid from '@material-ui/core/Grid'
 
 const SlaveTable = ({ slaves }) => {
   return (
-    <TableContainer>
+    <TableContainer style={{marginTop: 30}}>
       <Table aria-label="simple table">
         <TableHead>
           <TableRow>
@@ -32,8 +34,21 @@ const SlaveTable = ({ slaves }) => {
 }
 
 const Slaves = () => {
+  const [filter, setFilter] = useState('')
   const dispatch = useDispatch()
   const slaves = useSelector(state => state.slaves)
+  const filteredSlaves = slaves.filter(s => {
+    if (s.name.toLowerCase().includes(filter.toLowerCase())) {
+      return true
+    }
+    if (s.address.toLowerCase().includes(filter.toLowerCase())) {
+      return true
+    }
+    if (s.status.toLowerCase().includes(filter.toLowerCase())) {
+      return true
+    }
+    return false
+  })
 
   useEffect(() => {
     slaveService
@@ -47,12 +62,22 @@ const Slaves = () => {
       })
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
   return (
     <div>
       <h1>domzzer / Slaves</h1>
-      <Button color="primary" variant="contained" component={Link} to="/slaves/new">add new slave</Button>
-      <h3>Slaves in Database</h3>
-      <SlaveTable slaves={slaves} />
+      <Grid container spacing={3} justify="space-around">
+        <Grid item xs={6} style={{display:"flex", justifyContent: "flex-start"}}>
+          <TextField onChange={handleFilterChange} fullWidth value={filter} placeholder="filter by keyword"/>
+        </Grid>
+        <Grid item xs={6} style={{display:"flex", justifyContent: "center"}}>
+          <Button color="primary" variant="contained" component={Link} to="/slaves/new">add new slave</Button>
+        </Grid>
+      </Grid>
+      <SlaveTable slaves={filteredSlaves} />
     </div>
   )
 }
