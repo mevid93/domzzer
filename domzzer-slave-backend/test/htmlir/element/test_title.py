6cb7249911fbae6f5422a13dc8ba47227169eda8
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-07-06 19:08:48 +0300

    hidden (global attribute) + tests

--- domzzer-slave-backend/test/htmlir/element/test_title.py
+++ domzzer-slave-backend/test/htmlir/element/test_title.py
@@ -43,7 +43,7 @@ class TestHTMLTitleElement(unittest.TestCase):
     def test_convert_works(self):
         random.seed(666)
         title = HTMLTitleElement(0)
-        expected_str = "<title tabindex=\"32767\" accesskey=\"r\" title=\"Nice attribute title\">https://github.com/mevid93/domzzer</title>"
+        expected_str = "<title spellcheck=\"false\" title=\"Attribute title with multiline\ntext\" accesskey=\"m\">https://github.com/mevid93/domzzer</title>"
         self.assertEqual(title.convert(), expected_str)
 
 
