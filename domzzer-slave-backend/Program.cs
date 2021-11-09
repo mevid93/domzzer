c08e3668e6737f00c3b35d72236c8d2dc318b93e
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2021-11-09 16:48:54 +0200

    Changed project structure for slave backend

--- domzzer-slave-backend/Program.cs
+++ domzzer-slave-backend/Program.cs
@@ -16,11 +16,13 @@ namespace SlaveAPI
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args) {
+            DotNetEnv.Env.Load();
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
--- domzzer-slave-backend/Startup.cs
+++ domzzer-slave-backend/Startup.cs
@@ -39,6 +39,8 @@ namespace SlaveAPI
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddScoped<IVulnerabilityRepository, SqlVulnerabilityRepository>();
+
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
